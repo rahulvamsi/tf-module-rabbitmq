98ff24259c73c4eecb2d2cb46a966c215c22ddc1
Raghu K <raghuk.vit@gmail.com>
2022-09-15 08:28:46 +0530

    Update tf-module-rabbitmq/

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   username     = element(split("/", data.aws_ssm_parameter.credentials.value), 0)
   password     = element(split("/", data.aws_ssm_parameter.credentials.value), 1)
   ssh_password = element(split("/", data.aws_ssm_parameter.ssh_credentials.value), 0)
-  ssh_username = element(split("/", data.aws_ssm_parameter.ssh_credentials.value), 0)
+  ssh_username = element(split("/", data.aws_ssm_parameter.ssh_credentials.value), 1)
 }
