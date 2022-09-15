ae47c8ef7749bc618de0faa5f871a5fd98964509
Raghu K <raghuk.vit@gmail.com>
2022-09-15 08:27:42 +0530

    Update tf-module-rabbitmq/

--- main.tf
+++ main.tf
@@ -43,9 +43,10 @@ resource "aws_ec2_tag" "name-tag" {
 resource "null_resource" "ansible-apply" {
   provisioner "remote-exec" {
     connection {
-      host     = aws_spot_instance_request.rabbitmq.private_ip
-      user     = local.ssh_username
-      password = local.ssh_password
+      host = aws_spot_instance_request.rabbitmq.private_ip
+      user = local.ssh_username
+      //password = local.ssh_password
+      password = "DevOps321"
     }
     inline = [
       //"ansible-pull -i localhost, -U https://github.com/raghudevopsb66/roboshop-mutable-ansible -e HOSTS=localhost -e APP_COMPONENT_ROLE=rabbitmq -e ENV=${var.env} -e RABBITMQ_PASSWORD=${local.password}"
