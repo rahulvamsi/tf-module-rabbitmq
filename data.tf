008293f35446b9d2b9e4d33585a99e1c3f6cf382
rahulvamsi <p.rahulvamsi@gmail.com>
2022-10-04 20:12:44 +0530

    updated

--- data.tf
+++ data.tf
@@ -8,7 +8,7 @@ data "aws_ssm_parameter" "ssh_credentials" {
 
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "centos7-devops-practice-with-ansible"
+  name_regex  = "ansible_installed"
   owners      = ["self"]
 }
 
